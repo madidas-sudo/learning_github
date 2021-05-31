bab1a796c4d3a6d96ba85e76a8a16295ba077daf
Marius <marius0503@gmail.com>
2021-05-31 21:12:56 +0200

    added plusOne function added test of plusOne function to main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def plusOne(arr):
+    for i, j in enumerate(arr):
+        arr[i] = j + 1
+
 def main():
-    pass
+    testArr = [25, 12, 64, 12]
+    plusOne(testArr)
+    print(testArr)
+
+if __name__ == '__main__':
+    main()
